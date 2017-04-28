1df00349616e343a7d3674a1660dd29207bee2b4
Worapol Boontanonda <agentzh2m@gmail.com>
2017-04-28 16:09:42 +0700

    Add log and kind of finish

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ instances at the same time is very expensive
 * then you configure it by running `aws configure` and then put in your
 access credential id and secret key you can access from the credential detail when you click
 on your account
+
+* Go to the security group of all Asia region and then configure
+the default security group to let
+any ip in if you are scare we need ssh access from inbound and ping access
+from outbound
+
+* Furthermore add your public key to aws and name it whatever you want
+then put that name in the keyname variable (do that for every asia region)
--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import time
 import subprocess
 import sys
 
-ec2 = boto3.resource('ec2', region_name='ap-southeast-1')
 
-image_id = 'ami-fc5ae39f'
-keyname = 'hamuel-mbp' # go and add your public key in the aws console security
+# image_id = 'ami-fc5ae39f'
+keyname = 'hamuel-mbp'  # go and add your public key in the aws console security
 regions = {
-    'ap-northeast-1': 'Tokyo',
-    'ap-northeast-2': 'Seoul',
-    'ap-southeast-1': 'Singapore',
-    'ap-southeast-2': 'Sydney',
-    'ap-south-1': 'Mumbai'
+    'ap-northeast-1': ('Tokyo', 'ami-923d12f5'),
+    'ap-northeast-2': ('Seoul', 'ami-9d15c7f3'),
+    'ap-southeast-1': ('Singapore', 'ami-fc5ae39f'),
+    'ap-southeast-2': ('Sydney', 'ami-162c2575'),
+    'ap-south-1': ('Mumbai', 'ami-52c7b43d')
 }
 
 
@@ -38,46 +37,61 @@ def pinging(host):
         return ping.stderr.decode('utf-8').strip(), 'fail to ping the server'
 
 
-instances = ec2.create_instances(ImageId=image_id, MinCount=1,
-                                 MaxCount=1,
-                                 InstanceType='t2.micro',
-                                 KeyName=keyname,
-                                 # SecurityGroupIds=[
-                                 #     'sg-dbcf87bc'
-                                 # ],
-                                 BlockDeviceMappings=[{
-                                     'DeviceName': '/dev/sdf',
-                                     'VirtualName': 'ephemeral0',
-                                     'Ebs': {
-                                         'VolumeSize': 8,
-                                         'DeleteOnTermination': True,
-                                         'Encrypted': False
-                                     }
-                                 }]
-                                 )
-
 # get current instance
-print('Lanuching!! -->', instances)
-before_start = 0
-instance = instances[0]
-while instance.state['Name'] != 'running':
-    time.sleep(1)
-    before_start += 1
-    instances = [i for i in ec2.instances.filter(InstanceIds=[instance.id])]
+
+def runner(region, img):
+    ec2 = boto3.resource('ec2', region_name=region)
+    instances = ec2.create_instances(ImageId=img, MinCount=1,
+                                     MaxCount=1,
+                                     InstanceType='t2.micro',
+                                     KeyName=keyname,
+                                     # SecurityGroupIds=[
+                                     #     'sg-dbcf87bc'
+                                     # ],
+                                     BlockDeviceMappings=[{
+                                         'DeviceName': '/dev/sdf',
+                                         'VirtualName': 'ephemeral0',
+                                         'Ebs': {
+                                             'VolumeSize': 8,
+                                             'DeleteOnTermination': True,
+                                             'Encrypted': False
+                                         }
+                                     }]
+                                     )
+    print('Lanuching!! -->', instances)
+    before_start = 0
     instance = instances[0]
+    while instance.state['Name'] != 'running':
+        time.sleep(1)
+        before_start += 1
+        instances = [i for i in ec2.instances.filter(
+            InstanceIds=[instance.id])]
+        instance = instances[0]
 
-print('launch time ', before_start)
+    print('launch time ', before_start)
 
-host = 'ec2-user@' + instance.public_dns_name
-print('try to connect to', host)
+    host = 'ec2-user@' + instance.public_dns_name
+    print('try to connect to', host)
 
-print('Waiting for everything to be ready')
-time.sleep(15)
-print(pinging(instance.public_dns_name))
-print('Waiting for ssh server to be up')
-time.sleep(30)
-print(ssh_cmd(host, ["ping", "-c", "15", "www.google.com"]))
-print(ssh_cmd(host, ["ping", "-c", "15", "www.pantip.com"]))
+    print('Waiting for everything to be ready')
+    time.sleep(15)
+    print(pinging(instance.public_dns_name))
+    print('Waiting for ssh server to be up')
+    time.sleep(30)
+    print(ssh_cmd(host, ["ping", "-c", "15", "www.google.com"]))
+    print()
+    print(ssh_cmd(host, ["ping", "-c", "15", "www.pantip.com"]))
 
-print(instance.terminate())
-print('Terminate the instance after executing all command')
+    print(instance.terminate())
+    print('Terminate the instance in region', region, 'after executing all command')
+    return before_start
+
+starting_times = []
+for r in regions:
+    name, img = regions[r]
+    print('Testing region' , name)
+    starting_times.append(runner(r, img))
+    print('=' * 100)
+
+print('average starting time is', sum(starting_times) / float(len(starting_times)))
+print('The whole test is finish!!! yeh')
